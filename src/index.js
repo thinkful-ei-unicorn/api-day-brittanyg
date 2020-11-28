ff8cabb8ae9e3215bd7af26f252c0b096c684d25
Brittany Garrett <bgarrett@Brittanys-MBP.hsd1.tn.comcast.net>
2020-11-28 15:35:25 -0600

    new files and updates

--- src/index.js
+++ src/index.js
@@ -1,11 +1,24 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-console */
 import $ from 'jquery';
 
 import 'normalize.css';
 import './index.css';
 
+import api from './api';
+import store from './store';
 import shoppingList from './shopping-list';
 
+
+
 const main = function () {
+  api.getItems()
+    .then(res => res.json())
+    .then((items) => {
+      items.forEach((item) => store.addItem(item));
+      shoppingList.render();
+    });
+    
   shoppingList.bindEventListeners();
   shoppingList.render();
 };
